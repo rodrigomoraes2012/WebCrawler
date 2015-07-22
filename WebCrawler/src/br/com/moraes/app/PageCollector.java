fd2cc0dc696c35710c0948f308ca5fd7a2443351
Rodrigo Moraes <rodrigo.moraes2012@gmail.com>
2015-07-22 13:41:05 -0300

    Aula 1, JSoup Setup e teste incial.

---
+++ WebCrawler/src/br/com/moraes/app/PageCollector.java
@@ -0,0 +1,31 @@
+package br.com.moraes.app;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class PageCollector 
+{
+	public static void main(String[] args) 
+	{
+		
+		try 
+		{
+			Document doc = Jsoup.connect("http://www.jsoup.org/").get();
+			Elements links = doc.select("a");
+			
+			for(Element e: links)
+			{
+					System.out.println(e.attr("abs:href"));
+			}
+			
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+}
